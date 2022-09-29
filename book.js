fea7574c19e086f349c7bb76aa8349b5227fe5fc
Kavyasree26 <96222009+Kavyasree26@users.noreply.github.com>
2022-09-29 19:42:52 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+const bookschema=new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    purchased:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+}
+)
+module.exports=mongoose.model('book1',bookschema)
